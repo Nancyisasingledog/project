a5bcbd89d29392e51a879598e6cc72180a2c029d
nancy <cappy13566@gmail.com>
2019-04-12 12:00:54 +0800

    0412

--- GMT/05_work/05_work.sh
+++ GMT/05_work/05_work.sh
@@ -9,7 +9,7 @@ gmt makecpt -T0/25/5 -Cjet -Z > $cpt
 
 gmt psxy -R0/1/0/1 -JX1c -T -K -P > $out_ps
 gmt psbasemap -R119/123/21/26 -Jm3 -BwEsN -B1 -G187/255/255 -X-0.5 -Y5 -O -K>> $out_ps
-gmt psbasemap -R -J -B+t0412Rainfall -O -K>> $out_ps
+gmt psbasemap -R -J -B+t190412Rainfall -O -K>> $out_ps
 gmt pscoast -R -J -B -W0.5 -G255/250/205 -Df -Ia -O -K >> $out_ps
 awk '{if ($3>=0 && $3<10) print $1, $2, $3 }' $input_list | gmt psxy -R -J -Sc0.05 -C$cpt -O -K >> $out_ps
 awk '{if ($3>=10 && $3<15) print $1, $2, $3 }' $input_list | gmt psxy -R -J -Sc0.07 -W0.001 -C$cpt -O -K >> $out_ps
